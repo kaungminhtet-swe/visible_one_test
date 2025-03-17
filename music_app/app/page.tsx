0b813c291b6ac1bcd87656328c8f008cda0cac23
Kaung Min Htet <kgminhtet.dev@gmail.com>
2025-03-17 14:01:09 +0700

    feat(Music App): project setup

---
+++ music_app/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="flex min-w-screen min-h-screen items-center justify-center">
+      <Button variant={"outline"}>Music App Project Setup</Button>
+    </main>
+  );
+}
